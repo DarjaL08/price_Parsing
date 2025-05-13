bd0847ecc267275c74a0e4660a6c94c86ba28e0e
Jekaterina Ivanova <Jekaterina.Ivanova_9@edu.rtu.lv>
2025-05-13 23:31:50 +0300

    Add files via upload

---
+++ Kristers.py
@@ -0,0 +1,74 @@
+import requests
+from bs4 import BeautifulSoup
+
+def clean_text(text):
+    return ''.join(char for char in text if char.isalnum() or char.isspace())
+
+def get_price_kristers(brand, product):
+    output_file = "ksisters_results.txt"
+    results = [] # Список для хранения результатов
+
+
+    url = f"https://ksisters.lv/en/brand/{brand}"
+    response = requests.get(url)
+
+
+    if response.status_code != 200:
+        result = {"name": None, "price": f"Could not load {url}. Code: {response.status_code}"}
+        results.append(result)
+        with open(output_file, "a", encoding="utf-8") as f: # открываем файл для записи и записываем туда об ошибке
+            f.write(f"{result['price']}\n")
+        return results
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    product_cleaned = clean_text(product.lower().strip())
+
+
+    products = soup.find_all("div", class_="product-card-text")
+    if not products:
+        result = {"name": None, "price": f"Could not find the searched information for '{brand}'"}
+        results.append(result)
+        with open(output_file, "a", encoding="utf-8") as f:
+            f.write(f"{result['price']}\n")
+        return results
+
+   
+    for item in products:
+        name_tag = item.find("a", class_="product-name product-name-card")
+        price_tag = item.find("div", class_="product-card-price")
+        if name_tag and price_tag:
+            name_text = name_tag.get_text(strip=True)
+            name_text_cleaned = clean_text(name_text.lower())
+            if product_cleaned in name_text_cleaned:
+                result = {"name": name_text, "price": price_tag.get_text(strip=True)}
+                results.append(result)
+
+    # Если ничего не найдено, добавляем сообщение
+    if not results:
+        result = {"name": None, "price": f"The searched product '{product}' wasn't found on the page"}
+        results.append(result)
+
+    # Запись результатов в файл
+    with open(output_file, "a", encoding="utf-8") as f:
+        for result in results:
+            if result["name"]:
+                f.write(f"Found: {result['name']}, Price: {result['price']}\n")
+            else:
+                f.write(f"{result['price']}\n")
+    
+    return results
+
+if __name__ == "main":
+    
+    brand = input("Enter brand name, please: ")
+    product = input("Enter the title of the product, please: ")
+    
+    # Запуск парсинга
+    results = get_price_kristers(brand, product)
+    
+    # Вывод результатов в консоль
+    for result in results:
+        if result["name"]:
+            print(f"Found: {result['name']}, Price: {result['price']}")
+        else:
+            print(f"Error: {result['price']}")
